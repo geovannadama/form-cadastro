e286203656131bb30d1bc08470731816418acf72
Geovanna Dama <geovannadama9987@gmail.com>
2023-04-25 08:29:01 -0300

    atualização

--- cad.js
+++ cad.js
@@ -1,71 +1,14 @@
-function Verificar(){
-
-
-
+function Verificar() {
     let nome = document.getElementById('nome').value;
+    let telefone = document.getElementById('telefone').value;
+    let nascimento = document.getElementById('nascimento').value;
+    let cpf = document.getElementById('cpf').value;
+    let endereco = document.getElementById('endereco').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    let csenha = document.getElementById('csenha').value;
 
-   
-
-     let telefone = document.getElementById('telefone').value;
-
-   
-
-       let nascimento = document.getElementById('nascimento').value;
-
-   
-
-       let cpf = document.getElementById('cpf').value;
-
-   
-
-       let endereco = document.getElementById('endereco').value;
-
-   
-
-       let email = document.getElementById('email').value;
-
-   
-
-        let senha = document.getElementById('senha').value;
-
-   
-
-        let csenha = document.getElementById('csenha').value;
-
-   
-
-   
-
-   
-
-   
-
-        if(!email || !senha)
-
-   
-
+    if(!email || !senha || !csenha || !endereco || !cpf || !nascimento || !telefone || !nome){
         alert("Campos de preenchimento obrigatório. Favor preencher.")
-
-   
-
-        if(!csenha || !endereco)
-
-   
-
-        alert("Campos de preenchimento obrigatório. Favor preencher.")
-
-   
-
-        if(!cpf || !nascimento)
-
-   
-
-        alert("Campos de preenchimento obrigatório. Favor preencher.")
-
-   
-
-        if(!telefone || !nome)
-
-   
-
-        alert("Campos de preenchimento obrigatório. Favor preencher.") }
+    }
+}
